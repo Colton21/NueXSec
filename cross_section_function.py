35957f64b72abfce02c2f637cd15d7cca7ebf472
Colton Hill <chill@coltons-mbp.dhcp.fnal.gov>
2017-07-22 16:37:38 -0500

    stats and some sys errors

--- cross_section_function.py
+++ cross_section_function.py
@@ -18,8 +18,14 @@ def calcXSec(n_total, n_bkg, flux, efficiency):
     scale_factor = 2.4 * math.pow(10, 17)  # POT / nue
     # number of nucleons
     num_nucleons = 4.76 * math.pow(10, 31)
-
-    xsec_cc = (n_events * scale_factor) / (flux * num_nucleons * efficiency)
+    xsec_cc = []
+    xsec_cc.append((n_events * scale_factor) /
+                   (flux * num_nucleons * efficiency))
+    n_error = (n_events / np.sqrt(n_events))
+    xsec_cc.append((n_error * scale_factor) /
+                   (flux * num_nucleons * efficiency))
+    sys_error = float(xsec_cc[0]) * 0.30  # beam sys error of 30%
+    xsec_cc.append(sys_error)
 
     return xsec_cc
 
@@ -46,4 +52,4 @@ def printXsec(df, num_mc_cc_nue, flux):
     efficiency = float(num_pfp_cc_nue) / float(num_mc_cc_nue) * 100.
     xsec_cc = calcXSec(n_total, n_bkg, flux, efficiency)
 
-    print 'Nue CC Xsec: ', xsec_cc
+    print 'Nue CC Xsec: ', xsec_cc[0], ' +/- ', xsec_cc[1], ' +/- ', xsec_cc[2]
